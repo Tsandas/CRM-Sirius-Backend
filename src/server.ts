4e885b689b28724f869e0967b0a5bcb018ed6431
Tsandas <georgetsandekidis@gmail.com>
2025-12-05 19:46:58 +0200

    aws status heatlh check

--- src/server.ts
+++ src/server.ts
@@ -18,10 +18,10 @@ const limiter = rateLimit({
 });
 app.use("/", limiter);
 app.get("/health", (req, res) => {
-  res.status(200).send("OK");
+  res.json({ message: "Healthy" }).status(200).send("OK");
 });
 app.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.json({ message: "Healthy" }).status(200).send("OK");
 });
 
 app.use("/api/sysadmin/", sysadminRoutes);
